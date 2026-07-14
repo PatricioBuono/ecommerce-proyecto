8e9d1751269c16c0cf467f1d1f6f08007c5ab10f
Patricio Buono <patriciobuono5@gmail.com>
2026-07-14 13:17:04 -0300

    feat: integracion frontend-backend, configuracion CORS y renderizado del catalogo

---
+++ frontend/src/app/core/services/producto.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductoService{
+    
+    private apiUrl = 'http://localhost:8080/api/productos';
+
+    constructor(private http: HttpClient){}
+    
+    getProductos(): Observable<Producto[]>{
+        return this.http.get<Producto[]>(this.apiUrl);
+    }
+}
