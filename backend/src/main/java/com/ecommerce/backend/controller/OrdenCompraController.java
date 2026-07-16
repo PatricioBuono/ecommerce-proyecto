dfa31183606255b3c680086cd5efec74e2cca92e
Patricio Buono <patriciobuono5@gmail.com>
2026-07-16 11:03:04 -0300

    feat: implementacion de funcionalidad del botón confirmar compra

--- backend/src/main/java/com/ecommerce/backend/controller/OrdenCompraController.java
+++ backend/src/main/java/com/ecommerce/backend/controller/OrdenCompraController.java
@@ -6,10 +6,7 @@ import com.ecommerce.backend.service.OrdenCompraService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,6 +14,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/ordenCompra")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200/")
 public class OrdenCompraController {
 
     private final OrdenCompraService ordenCompraService;
